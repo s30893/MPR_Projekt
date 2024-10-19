6d3ab0bd00a1e26907b51c0d9e9cfd9959ebc0cc
Julia <s30893@pjestk.edu.pl>
2024-10-19 10:30:21 +0200

    Dodano sumę znaków

--- src/main/java/pl/edu/pjatk/MPR_Project/model/Kapibara.java
+++ src/main/java/pl/edu/pjatk/MPR_Project/model/Kapibara.java
@@ -12,12 +12,32 @@ public class Kapibara {
     private Long id;//2.
     private String name;
     private String color;
+    private Long identyfikator;
 
     public Kapibara(String name, String color) {
         this.name = name;
         this.color = color;
+        this.identyfikator = calcIdentyfikator();
     }
     public Kapibara(){} //5.
+
+    private Long calcIdentyfikator(){
+        long suma = 0;
+        for(char c: name.toCharArray()){
+            suma = suma + (int) c;
+        }
+        for(char c: color.toCharArray()){
+            suma = suma + (int) c;
+        }
+        return suma;
+    }
+    public Long getIdentyfikator() {
+        return identyfikator;
+    }
+
+    public void setIdentyfikator(Long identyfikator) {
+        this.identyfikator = identyfikator;
+    }
 
     public Long getId() {//2a.
         return id;
